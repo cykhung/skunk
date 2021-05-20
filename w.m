ceef053e23f0161970f4264ed2885f4b6a18d512
cykhung <45437973+cykhung@users.noreply.github.com>
2021-05-19 22:22:22 -0400

    Initial commit.

---
+++ w.m
@@ -0,0 +1,36 @@
+function w(filenames)
+
+if nargin == 0
+    filenames = '.';
+end
+if ischar(filenames)
+    filenames = {filenames};
+end
+
+
+for n = 1:numel(filenames)
+    filename = filenames{n};
+    if strcmp(filename, '.')
+        winopen('.');
+    elseif strcmp(filename(1:8), 'https://') || strcmp(filename(1:7), 'http://')
+        web(filename);
+    else    
+        filename = convert_filenames(filename);
+        [~, ~, ext] = fileparts(filename);
+        switch lower(ext)
+        case '.fig'
+            openfig(filenames{n});
+            plotedit on
+        case '.pdf'
+            sumatrapdf(filenames{n});
+        case {'.jpg', '.jpeg', '.png', '.svg', '.tif', '.nef'}
+            irfanview(filenames{n});
+        otherwise
+            winopen(filenames{n});
+        end
+    end
+end
+
+
+end
+
